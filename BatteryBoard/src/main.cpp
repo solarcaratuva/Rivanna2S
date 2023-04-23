283c2caecc9c282d100b25895aedf00349169702
Michael Fatemi <myfatemi04@gmail.com>
2023-04-23 18:32:23 +0000

    Add digitalouts

--- BatteryBoard/src/main.cpp
+++ BatteryBoard/src/main.cpp
@@ -15,57 +15,27 @@
 #define FLASH_PERIOD       500ms
 #define PRECHARGE_PAUSE    100ms
 
-// PowerAuxCANInterface vehicle_can_interface(MAIN_CAN_RX, MAIN_CAN_TX,
-                                        //    MAIN_CAN_STBY);
-// BPSCANInterface bps_can_interface(BMS_CAN1_RX, BMS_CAN1_TX, BMS_CAN1_STBY);
+DigitalIn aux_input(AUX_PLUS);
+DigitalIn dcdc_input(DCDC_PLUS);
+DigitalIn fantech_input(FanTech);
+DigitalIn contact12_input(CONTACT_12);
 
-bool flashHazards, flashLSignal, flashRSignal = false;
-Thread signalFlashThread;
+DigitalOut mppt_precharge(MPPT_PRECHARGE);
+DigitalOut motor_precharge(MOTOR_PRECHARGE);
+DigitalOut discharge_enable(DISCHARGE_ENABLE);
+DigitalOut charge_enable(CHARGE_ENABLE);
 
-AnalogIn fan_tach(FanTach);
-AnalogIn brake_light_current(BRAKE_LIGHT_CURRENT);
-AnalogIn headlight_current(DRL_CURRENT);
-AnalogIn bms_strobe_current(BMS_STROBE_CURRENT);
-AnalogIn left_turn_current(LEFT_TURN_CURRENT);
-AnalogIn right_turn_current(RIGHT_TURN_CURRENT);
-Thread peripheral_error_thread;
-
-BPSRelayController bps_relay_controller(HORN_EN, DRL_EN, AUX_PLUS,
-                                        BMS_STROBE_EN);
-
-// void peripheral_error_handler() {
-//     PowerAuxError msg;
-//     while (true) {
-//         msg.bps_strobe_error = (bms_strobe_current.read_u16() < 1000 &&
-//                                 bps_relay_controller.bps_fault_indicator_on());
-//         msg.brake_light_error =
-//             (brake_light_current.read_u16() < 1000 && brake_lights.read());
-//         msg.fan_error = (fan_tach.read_u16() < 1000);
-//         msg.left_turn_error =
-//             (left_turn_current.read_u16() < 1000 && leftTurnSignal.read());
-//         msg.right_turn_error =
-//             (right_turn_current.read_u16() < 1000 && rightTurnSignal.read());
-//         msg.bps_error = bps_relay_controller.bps_has_fault();
-
-//         vehicle_can_interface.send(&msg);
-//         ThisThread::sleep_for(ERROR_CHECK_PERIOD);
-//     }
-// }
-
-DigitalOut chargeRelay(CHARGE_RELAY);
-DigitalOut vbus(BUS_12V);
 Thread precharge_check;
 bool allow_precharge = true;
 
 
 void start_precharge() { //Enables switch to start precharging
-    chargeRelay = true;
+    charge_enable = true;
 }
-
 
 void battery_precharge() { 
     while (true) {
-        int relay_status = chargeRelay.read();
+        int relay_status = charge_enable.read();
         int vbus_status = vbus.read();
 
         if(relay_status && vbus_status && allow_precharge) {
