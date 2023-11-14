153015a196a13e8dd6a70a20614a916fa72ec4bf
Ethan Coon <eethanccoon@gmail.com>
2023-11-14 17:48:09 -0500

    Reimplementation of TokenBucket; tested

--- ECU/lib/src/ECUCANRateLimiter.cpp
+++ ECU/lib/src/ECUCANRateLimiter.cpp
@@ -8,9 +8,9 @@ std::tuple<int, bool> TokenBucket::past_interval_time(int last_time, int refill_
     if (last_time == 0) {
         log_debug("First Time for this CAN Message");
     }
-
+    
     const auto current_time = std::chrono::system_clock::now();
-    int current_time_seconds = std::chrono::duration_cast<std::chrono::seconds>(current_time.time_since_epoch()).count();
+    double current_time_seconds = std::chrono::duration_cast<std::chrono::milliseconds>(current_time.time_since_epoch()).count();
     if ((current_time_seconds - last_time) >= refill_rate) {
         last_time = current_time_seconds;
         past_interval = true;
@@ -21,3 +21,66 @@ std::tuple<int, bool> TokenBucket::past_interval_time(int last_time, int refill_
     }
     return std::make_tuple(last_time, past_interval);
 }
+
+// Re-implementation of TokenBucket, not yet formatted
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+class TokenBucket {
+public:
+    TokenBucket(int tokens, int time_unit, void (*forward_callback)(int), void (*drop_callback)(int)) {
+        this->tokens = tokens;
+        this->time_unit = time_unit;
+        this->forward_callback = forward_callback;
+        this->drop_callback = drop_callback;
+        this->bucket = static_cast<float>(tokens);
+        this->last_check = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    }
+
+    void handle(int packet) {
+        auto current = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+        auto time_passed = current - this->last_check;
+        this->last_check = current;        
+        this->bucket = this->bucket + time_passed * (static_cast<float>(this->tokens) / this->time_unit);
+        if (this->bucket > this->tokens) {
+            this->bucket = this->tokens;
+        }
+        if (this->bucket < 1) {
+            this->drop_callback(packet);
+        } else {
+            this->bucket -= 1;
+            this->forward_callback(packet);
+        }
+    }
+
+private:
+    int tokens;
+    int time_unit;
+    void (*forward_callback)(int);
+    void (*drop_callback)(int);
+    float bucket;
+    long long last_check;
+};
+
+void forward(int packet) {
+    std::cout << "Packet Forwarded: " << packet << std::endl;
+}
+
+void drop(int packet) {
+    std::cout << "Packet Dropped: " << packet << std::endl;
+}
+
+/* Main function for testing
+int main() {
+    TokenBucket throttle(1, 1000, forward, drop);
+    int packet = 0;
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        throttle.handle(packet);
+        packet += 1;
+    }
+    return 0;
+}
+*/
