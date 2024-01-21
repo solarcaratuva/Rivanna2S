79da0f7c18a02c65048c182057b44f2220f3a171
Andrew Amos <amosandrewj@gmail.com>
2024-01-21 14:43:48 -0500

    Added Rate Limiter to Motor Board

--- Motor/lib/src/MotorControllerCANInterface.cpp
+++ Motor/lib/src/MotorControllerCANInterface.cpp
@@ -36,7 +36,7 @@ int MotorControllerCANInterface::request_frames(bool power_status_frame,
 
     return result;
 }
-
+/*
 void MotorControllerCANInterface::message_handler() {
     while (true) {
         ThisThread::flags_wait_all(0x1);
@@ -68,6 +68,32 @@ void MotorControllerCANInterface::message_handler() {
         }
     }
 }
+*/
+void DriverCANInterface::message_handler() {
+    while (true) {
+        ThisThread::flags_wait_all(0x1);
+        CANMessage message;
+        while (can.read(message)) {
+            char message_data[17];
+
+            //TODO: Write to serial message_id, message_data
+
+            CANInterface::write_CAN_message_data_to_buffer(message_data,
+                                                           &message);
+            log_debug("Received CAN message with ID 0x%03X Length %d Data 0x%s ", message.id, message.len, message_data);
+            if (message.id == BPSError_MESSAGE_ID) {
+                BPSError can_struct;
+                can_struct.deserialize(&message);
+                handle(&can_struct);
+            } else if (message.id == MotorControllerPowerStatus_MESSAGE_ID) {
+                MotorControllerPowerStatus can_struct;
+                can_struct.deserialize(&message);
+                handle(&can_struct);
+            }
+        }
+    }
+}
+
 
 void MotorControllerCANInterface::check_bus_status() {
     while (true) {
