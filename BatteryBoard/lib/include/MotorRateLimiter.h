403959b16ce90f03565015a6d9adf339d9b50471
Ethan Coon <eethanccoon@gmail.com>
2024-01-21 15:30:44 -0500

    Added token bucket to battery board

---
+++ BatteryBoard/lib/include/MotorRateLimiter.h
@@ -0,0 +1,12 @@
+#include <mutex>
+#include <sstream>
+
+
+// Reimplementation
+class TokenBucket {
+public:
+    TokenBucket(int tokens, int time_unit);
+
+    // to-do: change int packet
+    void handle(CANMessage *message, uint16_t message_id)
+};
