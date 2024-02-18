3afb3879614fedd8f1aff4c67566a4d597742213
AlexTalreja <alextalreja@gmail.com>
2024-02-18 16:12:54 -0500

    Squashed commit of the following:
    commit 9c9e8ecd892f254acde327b19d455ede76e4d995
    Merge: 8ec5879 37cbd1c
    Author: AlexTalreja <alextalreja@gmail.com>
    Date:   Sun Feb 18 19:41:12 2024 +0000
    
        Merge branch 'pidupdate' of https://github.com/solarcaratuva/Rivanna2S into pidupdate
    
        Merge branch pidupdate
    
    commit 8ec58793ff75d961af35e7000e59ac550fe78c27
    Author: AlexTalreja <alextalreja@gmail.com>
    Date:   Sun Feb 18 19:40:35 2024 +0000
    
        reverted testing changes to real version
    
    commit 37cbd1c48e75aacde209ddc0e55945dd881a4bca
    Merge: 0173374 64ff844
    Author: AlexTalreja <113644271+AlexTalreja@users.noreply.github.com>
    Date:   Sun Feb 18 14:29:51 2024 -0500
    
        Merge pull request #4 from solarcaratuva/updatePinsREAL
    
        Merge updatePinsREAL into pidupdate
    
    commit 64ff84483c8715651a0a5034e00b90aa28c77a70
    Author: AlexTalreja <alextalreja@gmail.com>
    Date:   Sun Feb 18 19:20:09 2024 +0000
    
        removed FORWARD_REVIERSE_IN pindef
    
    commit 40a121f9c289b54cb24317fd61c0c1928f5e4a36
    Author: AlexTalreja <alextalreja@gmail.com>
    Date:   Sat Feb 10 17:47:47 2024 -0500
    
        updated new pins for driver board
    
    commit 0173374dd2d3f96d4b1ad7caf80daaaa03408ff7
    Author: AlexTalreja <alextalreja@gmail.com>
    Date:   Sun Feb 4 16:07:41 2024 -0500
    
        updated pid formula
    
    commit e487cfb6d48e0d2d4df8d15a79cb16b91fab9b63
    Author: Karthik Bhargav <karthikmbhargav@gmail.com>
    Date:   Sun Jan 28 14:45:50 2024 -0500
    
        delete clang-format
    
    commit b5754661cf145bf0f5f7fb9773bfdf19fbc822bd
    Merge: 40fc8a7 20ce285
    Author: Karthik Bhargav <40727862+karthikb19@users.noreply.github.com>
    Date:   Sun Jan 28 14:44:41 2024 -0500
    
        Merge pull request #3 from solarcaratuva/combiningDriverBoard
    
        Combining driver board
    
    commit 20ce285fec74279f95ba3ed1b8771a1e57cfdbaa
    Merge: ad7d90f 40fc8a7
    Author: Karthik Bhargav <40727862+karthikb19@users.noreply.github.com>
    Date:   Sun Jan 28 14:39:25 2024 -0500
    
        Merge branch 'pidupdate' into combiningDriverBoard
    
    commit ad7d90f16dae36e618ab012609aa209b2ae81eb5
    Author: AlexTalreja <alextalreja@gmail.com>
    Date:   Sun Jan 28 14:25:12 2024 -0500
    
        new steering wheel pindef.h and driverboard
    
    commit 40fc8a7ccc27b82e7100c9b00a9c1f2eaa6d9136
    Author: Karthik Bhargav <karthikmbhargav@gmail.com>
    Date:   Sat Jan 27 15:58:21 2024 -0500
    
        Add basic PID interface
    
    commit 54e5ca721e45e7c59a974af024c13fb8d29bc663
    Author: AlexTalreja <alextalreja@gmail.com>
    Date:   Sun Dec 3 16:13:46 2023 -0500
    
        combined powerAux and ECU boards

--- DriverBoard/include/pindef.h
+++ DriverBoard/include/pindef.h
@@ -3,25 +3,27 @@
 
 #ifdef TARGET_UVA_SOLAR_CAR
 
-#define LEFT_TURN_IN PB_1 // 17
-#define RIGHT_TURN_IN PB_14 // 28
-#define HAZARDS_IN PC_13 // 2
-#define MECHANICAL_BRAKE_IN PA_9 // 31
-#define REGEN_IN PB_15 // 29
-#define THROTTLE_VALUE_IN PB_13 // 27
-#define FORWARD_REVERSE_IN PB_2 // 18
+// #define FORWARD_REVERSE_IN PB_2
 
-#define CAN_RX             PA_11 // Rx for MCU
-#define CAN_TX             PA_12
-#define CAN_STBY           PB_10 // Digital Output
+#define CRUISE_ENABLED PB_14
+#define CRUISE_INC PB_13
+#define CRUISE_DEC PB_12
 
-#define LEFT_TURN_OUT PA_0 // 8
-#define RIGHT_TURN_OUT PA_1 // 9
-#define BRAKE_LIGHTS_OUT PB_12 // 26
-#define DRO_OUT PB_11 // 25
-#define BMS_STROBE_OUT PB_0 // 16
-#define THROTTLE PB_13 // 27
-#define REVERSE_IN PB_2 // 18
+#define LEFT_TURN_IN PA_10
+#define RIGHT_TURN_IN PA_9
+#define HAZARDS_IN PB_15
+#define MECHANICAL_BRAKE_IN PA_8
+#define REGEN_IN PB_11
+#define THROTTLE_VALUE_IN PB_2
+#define FORWARD_REVERSE_IN NC  // only one not done
+
+#define LEFT_TURN_OUT PA_2
+#define RIGHT_TURN_OUT PA_1
+#define BRAKE_LIGHTS_OUT PA_4
+#define DRO_OUT PA_0
+#define BMS_STROBE_OUT PA_3
+
+
 
 // #define BRAKE_LIGHT_IN PB_13
 // #define LEFT_TURN_LIGHT_IN PA_0
@@ -36,6 +38,10 @@
 // #define FORWARD PB_1
 
 #else
+
+#define CRUISE_ENABLED PB_14
+#define CRUISE_INC PB_13
+#define CRUISE_DEC PB_12
 
 #define LEFT_TURN_IN NC
 #define RIGHT_TURN_IN NC
