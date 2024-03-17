b64cf959241eb192f2fe1c1cbdd95cf5213e935a
bmatsuda <matsudbc@gmail.com>
2024-03-17 19:22:13 +0000

    finished testing for PowerAux 3/17

--- test/exampleTest.cpp
+++ test/exampleTest.cpp
@@ -3,8 +3,7 @@
 #include "../PowerAux/lib/include/TestSignalControl.h"
 #include "ethan.h"
  // Include your test implementation
-TestSignalControl testSignalControl; // Use your test implementation
-SignalFlashHandler signalFlashHandler{testSignalControl};
+// TestSignalControl testSignalControl; // Use your test implementation
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
@@ -12,8 +11,7 @@ int main(int argc, char **argv) {
 // Test fixture for SignalFlashHandler testing
 class SignalFlashHandlerTest : public ::testing::Test {
 protected:
-    TestSignalControl testSignalControl;
-    SignalFlashHandler signalFlashHandler{testSignalControl};
+    // TestSignalControl testSignalControl;
 
     // SetUp function to configure test conditions
     void SetUp() override {
@@ -29,19 +27,79 @@ protected:
 // Test case to check the functionality of SignalFlashHandler
 TEST_F(SignalFlashHandlerTest, TestSignalFlashHandlerFunctionality) {
     // Update state to simulate conditions
+    SignalFlashHandler signalFlashHandler;
     signalFlashHandler.updateState(false, true, true, false);
 
     // Set callbacks for sleep and wait
+
     signalFlashHandler.set_callbacks([](){}, [](){});
 
+    ASSERT_TRUE(signalFlashHandler.flashLSignal);
+    ASSERT_TRUE(signalFlashHandler.flashRSignal);
+
+}
+
+TEST_F(SignalFlashHandlerTest, TestSignalHandleFunctionalityLeftAndRightTurnSignal) {
+    // Update state to simulate conditions
+    SignalFlashHandler signalFlashHandler;
+    signalFlashHandler.updateState(false, false, true, false);
+
+    // Set callbacks for sleep and wait
+
+
+    signalFlashHandler.set_callbacks([](){}, [](){});
+
+    ASSERT_FALSE(signalFlashHandler.leftTurnSignal);
+    ASSERT_FALSE(signalFlashHandler.rightTurnSignal);
+
+    signalFlashHandler.handle();
+
+    ASSERT_FALSE(signalFlashHandler.leftTurnSignal);
+    ASSERT_TRUE(signalFlashHandler.rightTurnSignal);
     // Call the handle function
     //signalFlashHandler.handle();
 
     // Validate the expected behavior based on your implementation
     // You can use ASSERT_* or EXPECT_* macros to check expected results
-    // For example:
-    ASSERT_TRUE(testSignalControl.getFlashLeftSignalValue());
-    ASSERT_TRUE(testSignalControl.getFlashRightSignalValue());
 
+}
+
+TEST_F(SignalFlashHandlerTest, TestSignalHandleFunctionalityFlashHazards) {
+    //test flash hazards
+
+    // Update state to simulate conditions
+    SignalFlashHandler signalFlashHandler;
+    signalFlashHandler.updateState(true, false, false, false);
+
+    // Set callbacks for sleep and wait
+
+    signalFlashHandler.set_callbacks([](){}, [](){});
+
+    ASSERT_FALSE(signalFlashHandler.leftTurnSignal);
+    ASSERT_FALSE(signalFlashHandler.rightTurnSignal);
+
+    signalFlashHandler.handle();
+
+    ASSERT_TRUE(signalFlashHandler.leftTurnSignal);
+    ASSERT_TRUE(signalFlashHandler.rightTurnSignal);
+
+}
+
+TEST_F(SignalFlashHandlerTest, TestSignalHandleFunctionalityFlashBMS) {
+    //test flash hazards
+
+    // Update state to simulate conditions
+    SignalFlashHandler signalFlashHandler;
+    signalFlashHandler.updateState(false, false, false, true);
+
+    // Set callbacks for sleep and wait
+
+    signalFlashHandler.set_callbacks([](){}, [](){});
+
+    ASSERT_FALSE(signalFlashHandler.bms_strobe);
+
+    signalFlashHandler.handle();
+
+    ASSERT_TRUE(signalFlashHandler.bms_strobe);
 
 }
